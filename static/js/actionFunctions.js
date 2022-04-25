b1d1774e49835d9f983a34615e8b9e1191ca5606
Jeannot Maël <mael.jeannot@student-cs.fr>
2022-04-25 23:57:33 +0200

    design interface

--- static/js/actionFunctions.js
+++ static/js/actionFunctions.js
@@ -1,7 +1,7 @@
 function toggleForm() {
-    let formDiv = document.querySelector('#globalFormDiv')
-    if (formDiv.style.display === 'none')
-      formDiv.style.display = 'block';
-    else
-      formDiv.style.display = 'none';
+  let formDiv = document.querySelector('#globalFormDiv')
+  if (formDiv.style.display === 'none')
+    formDiv.style.display = 'block';
+  else
+    formDiv.style.display = 'none';
 }
